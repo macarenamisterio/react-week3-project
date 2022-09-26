df6520d331820a99c40c4dbad935119814f4b4db
macarenamisterio <macarena.misterio@gmail.com>
2022-09-26 21:30:59 +1000

    Adding return

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,13 @@
 export default function Search() {
-	<form>
-		<input
-			type="Search"
-			id="header-search"
-			placeholder="Search city"
-			name="search"
-		/>
-		<button type="submit">Search</button>
-	</form>;
+	return (
+		<form>
+			<input
+				type="Search"
+				id="header-search"
+				placeholder="Search city"
+				name="search"
+			/>
+			<button type="submit">Search</button>
+		</form>
+	);
 }
