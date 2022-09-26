8be936d9995d6c787975b372f9366b8028797314
macarenamisterio <macarena.misterio@gmail.com>
2022-09-26 21:28:22 +1000

    Adding search field and search button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
+import Search from "./Search.js";
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Weather Search</h1>
+			<Search />
 		</div>
 	);
 }
